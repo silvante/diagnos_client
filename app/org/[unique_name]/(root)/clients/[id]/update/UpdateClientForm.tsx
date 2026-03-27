19fdcf8f93584786c0b4d37babaf8b8ece0d8be5
silvante <khamidov.ko@gmail.com>
2026-03-27 14:56:44 +0500

    Warning

--- app/org/[unique_name]/(root)/clients/[id]/update/UpdateClientForm.tsx
+++ app/org/[unique_name]/(root)/clients/[id]/update/UpdateClientForm.tsx
@@ -196,10 +196,15 @@ export default function UpdateClientForm() {
   return (
     <div className="w-full space-y-4">
       <div className="text-muted-foreground text-sm hidden lg:flex gap-2">
-        <p>Formani tozalash</p>
-        <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
-          <span className="text-xs">Ctrl + Delete</span>
-        </kbd>
+        <p>
+          ⚠️ <strong>Diqqat:</strong> Mijoz ma’lumotlarini yangilashda ehtiyot
+          bo‘ling.
+          <strong>Tekshirilgan turlarni</strong> o‘chirish mumkin emas. Agar
+          barcha turlar <strong>tekshirilgan</strong> holda bo‘lsa, mijoz <strong>to‘liq tekshirilgan</strong> deb belgilanadi.
+          Bunday holatda mijoz ma’lumotlarini{" "}
+          <strong>o‘chirish yoki qayta tahrirlash </strong>
+          imkoniyati mavjud bo‘lmaydi.
+        </p>
       </div>
       {error !== "" && (
         <Alert variant="destructive">
