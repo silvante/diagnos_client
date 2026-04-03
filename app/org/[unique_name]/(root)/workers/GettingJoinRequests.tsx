b0b1e07440463f1668c1085ad466c004f3ffd7fb
silvante <khamidov.ko@gmail.com>
2026-04-03 20:57:07 +0500

    Basic design is ready

--- app/org/[unique_name]/(root)/workers/GettingJoinRequests.tsx
+++ app/org/[unique_name]/(root)/workers/GettingJoinRequests.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Heading from "@/app/(global_components)/Heading";
+import { BellDot } from "lucide-react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -7,13 +8,19 @@ export default function GettingJoinRequests() {
   const { organization } = useSelector((state: any) => state.validator);
   const [loading, setLoading] = useState(true);
 
-  //   async function (params:type) {
-
-  //   }
+    async function GetJoinReqs() {
+      
+    }
 
   return (
     <div className="border border-gray-300 p-5 border-b-4 border-b-violet-600 rounded-xl space-y-3">
-      <Heading text="Umumiy" />
+      <div className="w-full flex items-center justify-between">
+        <div className="flex gap-2 items-center">
+          <BellDot color="#7c3aed" />
+          <Heading text="Xabarlar" />
+        </div>
+        <Heading text={`(${0})`} />
+      </div>
     </div>
   );
 }
